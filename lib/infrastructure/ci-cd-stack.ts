5e0205d553d3037a6b2da68ceae03225cf3bd4f8
Kifah Abbad <kifahabbad@Kifahs-MBP.fritz.box>
2023-07-22 00:43:04 +0200

    code optimization

--- lib/infrastructure/ci-cd-stack.ts
+++ lib/infrastructure/ci-cd-stack.ts
@@ -1,17 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
-import {AppStage} from "./backend-stage-stack";
+import {AppStage} from "./app-stage-stack";
 
 export class CiCdStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const pipeline = new CodePipeline(this, 'Pipeline', {
-            pipelineName: 'MyPipeline',
+            pipelineName: 'TestPipeline',
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('Kifah/cdl-serverless-project', 'main'),
-                commands: ['npm ci', 'npm run build', 'npx cdk synth']
+                commands: ['npm ci', 'npm run build','npm run test', 'npx cdk synth']
             })
         });
         pipeline.addStage(new AppStage(this, "test", {}));
