b3417f1d0a5d17e4ceca773c54b06f84928c3f31
Kifah Abbad <kifahabbad@Kifahs-MBP.fritz.box>
2023-07-21 13:26:18 +0200

    minor changes

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -14,7 +14,6 @@ export class BackendStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        //add dynamoDB table and give it streaming and permissions needed to forward messages to pre-process lambda
         this.backendTable = new dynamodb.Table(this, 'CdkPlayBackendTable', {
             partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
