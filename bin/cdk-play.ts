3b1c1f99cc64dcef9b3812d2342f52e0410c9e97
Kifah Abbad <kifahabbad@Kifahs-MBP.fritz.box>
2023-07-22 00:15:05 +0200

    refined code

--- bin/cdk-play.ts
+++ bin/cdk-play.ts
@@ -3,9 +3,11 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import {PostprocessStack} from '../lib/postprocess-stack';
 import {BackendStack} from "../lib/backend-stack";
+import {CiCdStack} from "../lib/ci-cd-stack";
 // import {AwsSolutionsChecks} from "cdk-nag";
 
 const app = new cdk.App();
+new CiCdStack(app, 'CiCdStack');
 const backendStack = new BackendStack(app, 'BackendStack', {});
 new PostprocessStack(app, 'PostProcessStack', {backendTable: backendStack.backendTable});
 
