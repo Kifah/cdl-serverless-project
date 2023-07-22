4993eb8c8906cde6a961eda1fa94ec7e859544fb
Kifah Abbad <kifahabbad@Kifahs-MBP.fritz.box>
2023-07-22 04:05:58 +0200

    refined lambda rights

--- resources/postprocess/postprocess.ts
+++ resources/postprocess/postprocess.ts
@@ -6,18 +6,32 @@ const client = new AppConfigClient({region: "eu-central-1"});
 
 async function handler(event: SQSEvent) {
 
-    const input = { // GetConfigurationRequest
-        Application: "issues-app", // required
-        Environment: "dev", // required
-        Configuration: "notification-information", // required
-        ClientId: "STRING_VALUE", // required
-        ClientConfigurationVersion: "2",
+    const input = {
+        "Application": "issues-app",
+        "ClientId": "example-id",
+        "Configuration": "notification-information",
+        "Environment": "dev",
+        "ConfigurationVersion": "3"
     };
 
     const command = new GetConfigurationCommand(input);
     const response = await client.send(command);
-    console.log(JSON.stringify(event));
-    console.log(event.Records[0].body);
+
+    // This fires after the blob has been read/loaded.
+    const reader = new FileReader();
+    reader.addEventListener('loadend', (e) => {
+        // @ts-ignore
+        const text = e.srcElement.result;
+        console.log(text);
+    });
+
+// Start reading the blob as text.
+    // @ts-ignore
+    reader.readAsText(response.Content);
+
+
+    //console.log(JSON.stringify(event));
+    //console.log(event.Records[0].body);
 }
 
 export {handler}
