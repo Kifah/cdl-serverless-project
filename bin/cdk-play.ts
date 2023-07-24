96b49e620a0501796b46977fc2ddabfcefc47d40
Kifah Abbad <kifahabbad@Kifahs-MBP.fritz.box>
2023-07-24 10:24:33 +0200

    redeploy

--- bin/cdk-play.ts
+++ bin/cdk-play.ts
@@ -5,11 +5,10 @@ import {DeployEnv, PostprocessStack} from '../lib/application/postprocess-stack'
 import {BackendStack} from "../lib/application/backend-stack";
 import {CiCdStack} from "../lib/infrastructure/ci-cd-stack";
 import 'dotenv/config'
-require('dotenv').config({ override: true })
 
+require('dotenv').config({override: true})
 
 // import {AwsSolutionsChecks} from "cdk-nag";
-
 const app = new cdk.App();
 const envEU = {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION};
 
