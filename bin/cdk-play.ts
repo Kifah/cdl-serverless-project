2a3b486787b1adc2c9a779691eb051dd2325628c
Kifah Abbad <kifahabbad@Kifahs-MBP.fritz.box>
2023-07-24 11:34:04 +0200

    removed tests

--- bin/cdk-play.ts
+++ bin/cdk-play.ts
@@ -5,12 +5,11 @@ import {DeployEnv, PostprocessStack} from '../lib/application/postprocess-stack'
 import {BackendStack} from "../lib/application/backend-stack";
 import {CiCdStack} from "../lib/infrastructure/ci-cd-stack";
 import 'dotenv/config';
-
+// import {AwsSolutionsChecks} from "cdk-nag";
 require('dotenv').config();
 
 const envEU = {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION};
 
-// import {AwsSolutionsChecks} from "cdk-nag";
 const app = new cdk.App();
 
 new CiCdStack(app, 'CiCdStack', {env: envEU});
